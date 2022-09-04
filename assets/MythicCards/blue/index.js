0d56d8996048cd6d2787adebe91adc6d5f50285c
fogarea <shiitakedd@gmail.com>
2022-09-05 01:25:07 +0300

    feat: add src files

---
+++ assets/MythicCards/blue/index.js
@@ -0,0 +1,29 @@
+import blue1 from './blue1.webp';
+import blue2 from './blue2.webp';
+import blue3 from './blue3.webp';
+import blue4 from './blue4.webp';
+import blue5 from './blue5.webp';
+import blue6 from './blue6.webp';
+import blue7 from './blue7.webp';
+import blue8 from './blue8.webp';
+import blue9 from './blue9.webp';
+import blue10 from './blue10.webp';
+import blue11 from './blue11.webp';
+import blue12 from './blue12.webp';
+
+const cards = {
+  blue1,
+  blue2,
+  blue3,
+  blue4,
+  blue5,
+  blue6,
+  blue7,
+  blue8,
+  blue9,
+  blue10,
+  blue11,
+  blue12,
+}
+
+export default cards
---
+++ assets/MythicCards/green/index.js
@@ -0,0 +1,41 @@
+import green1 from './green1.webp';
+import green2 from './green2.webp';
+import green3 from './green3.webp';
+import green4 from './green4.webp';
+import green5 from './green5.webp';
+import green6 from './green6.webp';
+import green7 from './green7.webp';
+import green8 from './green8.webp';
+import green9 from './green9.webp';
+import green10 from './green10.webp';
+import green11 from './green11.webp';
+import green12 from './green12.webp';
+import green13 from './green13.webp';
+import green14 from './green14.webp';
+import green15 from './green15.webp';
+import green16 from './green16.webp';
+import green17 from './green17.webp';
+import green18 from './green18.webp';
+
+const cards = {
+  green1,
+  green2,
+  green3,
+  green4,
+  green5,
+  green6,
+  green7,
+  green8,
+  green9,
+  green10,
+  green11,
+  green12,
+  green13,
+  green14,
+  green15,
+  green16,
+  green17,
+  green18,
+};
+
+export default cards;
